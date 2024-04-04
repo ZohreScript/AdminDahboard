3ebd7fb2a1c3481de4467b4708f27b91913da0eb
zohreScript <z.omidi74@gmail.com>
2024-04-04 14:05:39 +0330

    err-coRS

--- src/features/identity/components/Register/Register.jsx
+++ src/features/identity/components/Register/Register.jsx
@@ -116,6 +116,6 @@ export default Register;
 export async function registerAction({ request }) {
   const formData = await request.formData();
   const data = Object.fromEntries(formData);
-  const response = await httpService.post('/users', data);
+  const response = await httpService.post('/Users', data);
   return response.status === 200;
 }
